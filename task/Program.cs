44e2bd6cfbaf8244a38f43b52693796acd3795f1
A.Islamshin <arshavin0511@gmail.com>
2022-09-09 15:32:57 +0300

    2 task

--- task/Program.cs
+++ task/Program.cs
@@ -3,11 +3,25 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
-int result = 1;
-for(int i =0; i < b; i++)
-{
-    result = result * a;
-}
-Console.WriteLine(result);
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// int result = 1;
+// for(int i =0; i < b; i++)
+// {
+//     result = result * a;
+// }
+// Console.WriteLine(result);
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// int result = 0;
+// while(n > 0) 
+// {
+// result = result + n % 10;
+// n /= 10;
+// }
+// Console.WriteLine(result);
