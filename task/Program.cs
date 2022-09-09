692cba1763fa7189fd96051f7a14bbe8e40abbbb
A.Islamshin <arshavin0511@gmail.com>
2022-09-09 15:46:55 +0300

    3 task

--- task/Program.cs
+++ task/Program.cs
@@ -25,3 +25,13 @@
 // n /= 10;
 // }
 // Console.WriteLine(result);
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+// int [] array = new int [8];
+// int n = 0;
+// for (int i = 0; i < array.Length; i++)
+// { n = Convert.ToInt32(Console.ReadLine());
+//     array[i] = n;
+// }
+// Console.WriteLine("[" + string.Join(", ", array) + "]");
